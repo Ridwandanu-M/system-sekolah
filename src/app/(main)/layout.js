9c57ffcd7c28db15f15072ac090dd6d494ae8621
Ridwandanu <danumaulana425@gmail.com>
2025-10-04 15:07:37 +0700

    installing shadcn

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "School System",
+  description: "School system created by Ridwandanu Maulana with Next.js",
+};
+
+export default function HomeLayout({ children }) {
+  return (
+    <section>
+      <main>{children}</main>
+    </section>
+  );
+}
