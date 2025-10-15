2f79685501ca0ae00ed7b5addcfd5e68149a8b25
Ridwandanu <danumaulana425@gmail.com>
2025-10-15 20:56:02 +0700

    progres

--- src/ui/Achievement.js
+++ src/ui/Achievement.js
@@ -1,7 +1,28 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 const Achievement = () => {
   return (
-    <section>
-      <div></div>
+    <section className="bg-[var(--primary-color)]">
+      <div className="max-w-[120rem] mx-auto py-[10.8rem]">
+        <h2 className="text-center text-[3.2rem] font-[700] text-[#fff]">
+          Prestasi
+        </h2>
+        <div>
+          <Carousel>
+            <CarouselContent>
+              <CarouselItem className="basis-1/3">...</CarouselItem>
+              <CarouselItem className="basis-1/3">...</CarouselItem>
+              <CarouselItem className="basis-1/3">...</CarouselItem>
+            </CarouselContent>
+          </Carousel>
+        </div>
+      </div>
     </section>
   );
 };
