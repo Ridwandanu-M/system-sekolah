b19820470e5986be50453e42db6077978ab493ef
RidwandanuM <danumaulana425@gmail.com>
2025-10-25 23:34:40 +0700

    progres

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -1,7 +1,12 @@
+import AdminSidebar from "@/components/AdminSidebar";
+
 export default function AdminLayout({ children }) {
   return (
-    <>
-      <main>{children}</main>
-    </>
+    <div className="flex">
+      <AdminSidebar />
+      <main className="w-[160rem] mx-auto py-[1.8rem] border-x">
+        {children}
+      </main>
+    </div>
   );
 }
---
+++ src/components/AdminSidebar.js
@@ -0,0 +1,139 @@
+"use client";
+
+import {
+  Menu,
+  House,
+  UsersRound,
+  GraduationCap,
+  Book,
+  LogOut,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+const AdminSidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [showText, setShowText] = useState(true);
+
+  const menus = [
+    {
+      id: 1,
+      menu: "Dashboard",
+      icon: (
+        <House
+          size={32}
+          className={`rounded-full p-[.4rem] cursor-pointer transition-all duration-[.075s] ease-in-out`}
+        />
+      ),
+      path: "/admin/dashboard",
+    },
+    {
+      id: 2,
+      menu: "Siswa",
+      icon: (
+        <UsersRound
+          size={32}
+          className={`rounded-full p-[.4rem] cursor-pointer transition-all duration-[.075s] ease-in-out`}
+        />
+      ),
+      path: "/admin/siswa",
+    },
+    {
+      id: 3,
+      menu: "Guru",
+      icon: (
+        <GraduationCap
+          size={32}
+          className={`rounded-full p-[.4rem] cursor-pointer transition-all duration-[.075s] ease-in-out`}
+        />
+      ),
+      path: "/admin/guru",
+    },
+    {
+      id: 4,
+      menu: "Placeholder",
+      icon: (
+        <Book
+          size={32}
+          className={`rounded-full p-[.4rem] cursor-pointer transition-all duration-[.075s] ease-in-out`}
+        />
+      ),
+      path: "/admin/placeholder",
+    },
+  ];
+
+  useEffect(() => {
+    if (isOpen) {
+      const timer = setTimeout(() => {
+        setShowText(true);
+      }, 30);
+
+      return () => clearTimeout(timer);
+    } else {
+      setShowText(false);
+    }
+  }, [isOpen]);
+
+  return (
+    <aside
+      className={`bg-[var(--primary-color)] h-screen ${isOpen ? "w-[25rem]" : "w-[7rem]"} py-[1.8rem] px-[1.5rem] transition-all duration-[.075s] eas-in-out flex flex-col`}
+    >
+      <div className={`flex ${isOpen && "justify-between"} items-center`}>
+        <Image
+          src="/Logo_Seyegan.png"
+          width={40}
+          height={40}
+          alt="Logo Seyegan"
+          className={`hover:bg-[var(--primary-color-tint-lighter)] rounded-full p-[.4rem] cursor-pointer transition-all duration-[.075s] ease-in-out`}
+          onClick={() => setIsOpen((prev) => !prev)}
+        />
+        <Menu
+          color="#fff"
+          size={40}
+          className={`hover:bg-[var(--primary-color-tint-lighter)] ${isOpen ? "block" : "hidden"} rounded-full p-[.4rem] cursor-pointer transition-all duration-[.075s] ease-in-out`}
+          onClick={() => setIsOpen((prev) => !prev)}
+        />
+      </div>
+      <div className="mt-[3.2rem]">
+        <ul className="flex flex-col gap-[1.2rem]">
+          {menus.map((item, index) => (
+            <li key={item.id}>
+              <Link
+                href={item.path}
+                className="flex items-center text-[1.6rem] text-[#fff] hover:bg-[var(--primary-color-tint-lighter)] py-[.4rem] rounded-xl transition-all duration-[.075s] ease-in-out"
+              >
+                {item.icon}
+                <p
+                  className={`${isOpen && showText ? "block" : "hidden"} pl-[.8rem] font-[600] transition-opacity duration-150 ease-in-out ${isOpen && showText ? "opacity-100" : "opacity-0"}`}
+                >
+                  {item.menu}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="mt-auto">
+        <Link
+          href="/sign-in"
+          className="flex items-center text-[1.6rem] text-[#fff] hover:bg-[var(--primary-color-tint-lighter)] py-[.4rem] rounded-xl transition-all duration-[.075s] ease-in-out"
+        >
+          <LogOut
+            size={32}
+            className={`rounded-full p-[.4rem] cursor-pointer transition-all duration-[.075s] ease-in-out`}
+          />
+          {isOpen && (
+            <p
+              className={`${isOpen && showText ? "block" : "hidden"} pl-[.8rem] font-[600] transition-opacity duration-150 ease-in-out ${isOpen && showText ? "opacity-100" : "opacity-0"}`}
+            >
+              Keluar
+            </p>
+          )}
+        </Link>
+      </div>
+    </aside>
+  );
+};
+
+export default AdminSidebar;
